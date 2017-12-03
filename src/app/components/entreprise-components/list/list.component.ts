8b816c54be94e761b7a335ae0e3c941a99eb1894
mohamedhedi.basly@esprit.tn <mohamedhedi.basly@esprit.tn>
2017-12-03 10:20:20 +0100

    2 affichages  suppression ajouter et recherche avec css et html

--- src/app/components/entreprise-components/list/list.component.ts
+++ src/app/components/entreprise-components/list/list.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Entreprise} from '../../../model/entrepriseModel/entreprise';
+import { EntrepriseService } from '../../../service/entrepriseService/entreprise.service';
+import { Http, Response} from '@angular/http';
+import { FilterPipe } from '../../../filter.pipe';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+entreprises: Entreprise[] = [];
+  constructor(private service:EntrepriseService) { }
 
   ngOnInit() {
+     var other = this;
+ this.service.getAllEntreprise().subscribe(e => this.entreprises = e)
+
+  }
+      
+deleteEntreprise(entreprise){
+    this.service.deleteEntreprise(entreprise).subscribe(deletedEntreprise=>{
+      console.log(deletedEntreprise);
+      let index=this.entreprises.indexOf(entreprise);
+
+      this.entreprises.splice(index,1);
+    
+    
+    })
+
+  
   }
 
 }
--- src/app/service/entrepriseService/entreprise.service.ts
+++ src/app/service/entrepriseService/entreprise.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 
@@ -8,17 +8,45 @@ export class EntrepriseService {
    
   url="http://localhost:18080/EasyMission-web/rest/entreprises";
   url1="http://localhost:18080/EasyMission-web/rest/entreprises/All";
+  urlRecruiter="http://localhost:18080/EasyMission-web/rest/recruiter";
+  urldetaille="http://localhost:18080/EasyMission-web/rest/entreprises/getEntrepriseById/";
+  urlaffecter="http://localhost:18080/EasyMission-web/rest/recruiter";
+  
+  
+
   constructor(private http: Http) { }
 
    getAllEntreprise() {
     return this.http.get(this.url1).map(resp => resp.json());
+  }
+
+  getRecruiterDisponible(){
+  return this.http.get(this.urlRecruiter).map(resp => resp.json());
+
   }
    createEntreprise(entreprise) {
 
     return this.http.post(this.url, entreprise).map(resp => resp.json());
   }
 
-  deleteEntreprise(entreprise) {
-    return this.http.delete(this.url + '/' + entreprise.id).map(resp => resp.json());
-  }
+     deleteEntreprise(ressource){
+      
+       return this.http.delete(this.url, new RequestOptions({
+        body: ressource
+       }))
+     
+      }
+
+      getEntrepriseById(id:number){
+        return this.http.get(this.urldetaille+id).map(resp=>resp.json());
+      }
+
+      recruiterInENtreprise(ressource){
+          return this.http.put(this.urlaffecter,new RequestOptions({
+            body:ressource
+          }))
+      }
+
+   
+
 }
